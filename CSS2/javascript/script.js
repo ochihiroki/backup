868ea0294950892128155c3cd3e8e1bd31e1f1d2
ochihiroki <h.o.0428.hoh@gmail.com>
2017-12-18 20:19:01 +0900

    コミットします。

---
+++ CSS2/javascript/script.js
@@ -0,0 +1,10 @@
+document.write(3*3);
+document.write(4/2);
+var a = 5+10;
+document.write(a);
+var b = 55-28;
+document.write(b);
+var g = 5*5;
+var h = 5*5;
+
+document.write(h+"="+g);
---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+var s1 = 2;
+if(a1<10){
+    document.write("A");
+}
+
+var a2 = 10;
+if(a2<= 10){
+   document.write("B");
+   }
