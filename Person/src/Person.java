d568b1a847193c4cbcccd6d9dbbb5a67ee227929
HikaruYagi <azezpztzwz@gmail.com>
2018-01-19 21:44:37 +0900

    1/19

---
+++ Person/src/Person.java
@@ -0,0 +1,16 @@
+
+public class Person {
+ public String name = null;
+ public int age =0;
+ public String PhoneNumber = null;
+ public String address = null;
+ public void talk(){
+	 System.out.println(this.name+"‚ª˜b‚·");
+ }
+ public  void walk(){
+	System.out.println(this.name+"‚ª•à‚­");
+}
+public void run(){
+	System.out.println(this.name+"‘–‚é");
+}
+}
---
+++ Person/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[]args){
+		Person hiroki = new Person();
+		hiroki.name = "‰z’q”Ž‹I";
+		hiroki.age= 28;
+		hiroki.talk();
+        hiroki.walk();
+        hiroki.run();
+		System.out.println(hiroki.name);
+		System.out.println(hiroki.age);
+
+	}
+
+}
